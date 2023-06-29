244adf827660f2846a4db237aacfb957002a662c
Artem <artem-agayan@mail.ru>
2023-06-29 13:27:43 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<script>
+import MainPage from './components/MainPage.vue';
+export default {
+  components: {MainPage},
+  data() {
+    return {
+      array: [
+        {
+          "name": "foo1",
+          "value": "bar1",
+        },
+        {
+          "name": "foo2",
+          "value": "bar2",
+        },
+        {
+          "name": "foo3",
+          "value": "bar3",
+        },
+        {
+          "name": "foo4",
+          "value": "bar4",
+        },
+        {
+          "name": "foo5",
+          "value": "bar5",
+        },
+        {
+          "name": "foo6",
+          "value": "bar6",
+        },
+        {
+          "name": "foo7",
+          "value": "bar7",
+        },
+        {
+          "name": "foo8",
+          "value": "bar8",
+        },
+        {
+          "name": "foo9",
+          "value": "bar9",
+        },
+        {
+          "name": "foo10",
+          "value": "bar10",
+        },
+        {
+          "name": "foo11",
+          "value": "bar11",
+        },
+        {
+          "name": "foo12",
+          "value": "bar12",
+        },
+        {
+          "name": "foo13",
+          "value": "bar13",
+        },
+        {
+          "name": "foo14",
+          "value": "bar14",
+        },
+        {
+          "name": "foo15",
+          "value": "bar15",
+        },
+        {
+          "name": "foo16",
+          "value": "bar16",
+        },
+        {
+          "name": "foo17",
+          "value": "bar17",
+        },
+        {
+          "name": "foo18",
+          "value": "bar18",
+        },
+        {
+          "name": "foo19",
+          "value": "bar19",
+        },
+        {
+          "name": "foo20",
+          "value": "bar20",
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+  </header>
+
+  <MainPage :array="array"/>
+</template>
+
+<style scoped>
+header {
+  padding: 10px 0;
+  display: flex;
+  gap: 20px;
+  width: 100%;
+}
+.logo {
+  height: 6em;
+  will-change: filter;
+  transition: filter 300ms;
+  width: 50px;
+  height: 50px;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                current: this.array[0],
+            },
+            inputValue: this.array[0].value,
+        }
+    },
+    props: ['array'],
+    methods: {
+        handleClick(e) {
+            this.form.current = this.array[e]
+            this.inputValue = this.array[e].value
+        },
+        onSubmit() {
+            this.form.current.value = this.inputValue
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Элементы массива:</h1>
+    <div class="wrapper">
+        <el-table :data="array" border style="width: fit-content">
+            <el-table-column prop="name" label="Name" width="180" />
+            <el-table-column prop="value" label="Value" width="180" />
+            <el-table-column fixed="right" label="Operations" width="120">
+                <template #default="scope">
+                    <el-button type="primary" size="small" @click.prevent="handleClick(scope.$index)">Edit Value</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <el-form label-width="120px" class="form">
+            <h2>{{ form.current.name }}</h2>
+            <label>
+                Value:
+                <el-input v-model="inputValue" />
+            </label>
+            <el-button class="form__btn" type="primary" @click="onSubmit">Save</el-button>
+        </el-form>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 24px;
+}
+.wrapper {
+    position: relative;
+    display: flex;
+    gap: 50px;
+    margin-bottom: 50px;
+}
+.form {
+    position: sticky;
+    top: 0;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: fit-content;
+}
+label {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-bottom: 20px;
+}
+h2 {
+    margin: 0 0 18px 0;
+    text-align: center;
+}
+</style>
